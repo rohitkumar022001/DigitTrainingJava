4d8e2636b3e65925623f6bd513e2d82839c020c1
Rohit.Kumar02 <Rohit.Kumar02@LDIGITBGL-L0215.godigit.corp>
2023-07-18 12:24:03 +0530

    This is my first commit

---
+++ src/main/java/com/digit/testProj/p1/TestingCode.java
@@ -0,0 +1,14 @@
+package com.digit.testProj.p1;
+
+public class TestingCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("rohit");
+		System.out.println("rohit");
+		System.out.println("rohit");
+		System.out.println("rohit");
+	}
+
+}
