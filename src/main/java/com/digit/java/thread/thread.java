870ff6a9311b8cb93116bcdd758d465bb69694b3
Rohit.Kumar02 <Rohit.Kumar02@LDIGITBGL-L0215.godigit.corp>
2023-07-19 11:01:12 +0530

    thread commit

---
+++ src/main/java/com/digit/java/thread/thread.java
@@ -0,0 +1,25 @@
+package com.digit.java.thread;
+
+import java.util.Scanner;
+
+public class thread {
+
+	public static void main(String[] args) throws InterruptedException{
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("banking operation");
+		System.out.println("enter account");
+		int acc=sc.nextInt();
+		System.out.println("enter pin");
+		int pin=sc.nextInt();
+		System.out.println("bANKING COMPLETED");
+		System.out.println("printing operation");
+		for(int i=0;i<6;i++) {
+			System.out.println("hii rohit");
+			Thread.sleep(3000);
+		}
+		System.out.println("printing completed");
+		
+	}
+
+}
