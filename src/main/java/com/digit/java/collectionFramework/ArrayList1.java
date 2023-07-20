7fee139288b1771ef7aa5ffa85c6450558932035
Rohit.Kumar02 <Rohit.Kumar02@LDIGITBGL-L0215.godigit.corp>
2023-07-20 12:40:10 +0530

    arraylist

---
+++ src/main/java/com/digit/java/collectionFramework/ArrayList1.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package com.digit.java.collectionFramework;
+import java.util.ArrayList;
+
+/**
+ * 
+ */
+public class ArrayList1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList l=new ArrayList();
+		l.add(200);
+		l.add(1, 300);
+		l.add("rohit");
+		l.add('a');
+		boolean x=l.contains(200);
+		System.out.println(x);
+		ArrayList l1=new ArrayList();
+		l1=(ArrayList)l.clone();
+		System.out.println(l);
+		System.out.println(l1);
+		//l.clear();
+		System.out.println(l);
+		System.out.println(l.get(2));
+		System.out.println(l1.indexOf(new Integer(300)));
+		System.out.println(l.isEmpty());
+		System.out.println(l.remove(2));
+		System.out.println(l.size());
+		System.out.println(l.toString());
+		System.out.println(l.hashCode());
+		System.out.println(l1.hashCode());
+		//System.out.println(l.addAll(1, l1));
+		//System.out.println(l);
+		l.add(200);
+		l.add(-1);
+		System.out.println(l);
+		//l.toArray();
+		System.out.println(l);
+		System.out.println(l.containsAll(l1));
+		
+
+	}
+
+}
